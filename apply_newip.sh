6456e21a88abc6462262a3202bee3ddcd4854a16
yangyanjun <ic-include@163.com>
2022-09-30 10:32:21 +0800

    SIG遗留问题修改 1、license文件以及代码文件头去掉“NewIP is dual licensed: you can use it under the terms of the GPL V2 and the BSD2 license”相关描述，只在项目仓根目录license文件中描述每个路径的license即可。 2、只保留根目录license文件，去掉子目录license文件。 3、NewIP是网络5.0联盟共同推出的新协议，属于公共，建议公共代码license使用BSD-3，不要使用BSD-2。liteOS是BSD-3，如果用BSD-2，属于反向使用。 4、linux目录增加README.OpenSource，相当于NewIP引用Linux内核第三方软件的形式，参考鸿蒙社区Linux-5.10内核仓README.OpenSource。 5、communication_sfc_newip/code 改 src。
    Signed-off-by: yangyanjun <yangyanjun@huawei.com>

--- apply_newip.sh
+++ apply_newip.sh
@@ -18,39 +18,39 @@ function main()
 {
 	cd $KERNEL_BUILD_ROOT
 
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/linux/newip_route.h            $KERNEL_BUILD_ROOT/include/linux/newip_route.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/linux/nip.h                    $KERNEL_BUILD_ROOT/include/linux/nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/linux/nip_icmp.h               $KERNEL_BUILD_ROOT/include/linux/nip_icmp.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/linux/newip_route.h            $KERNEL_BUILD_ROOT/include/linux/newip_route.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/linux/nip.h                    $KERNEL_BUILD_ROOT/include/linux/nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/linux/nip_icmp.h               $KERNEL_BUILD_ROOT/include/linux/nip_icmp.h
 	
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/netns/nip.h                $KERNEL_BUILD_ROOT/include/net/netns/nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/flow_nip.h                 $KERNEL_BUILD_ROOT/include/net/flow_nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/if_ninet.h                 $KERNEL_BUILD_ROOT/include/net/if_ninet.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/ninet_connection_sock.h    $KERNEL_BUILD_ROOT/include/net/ninet_connection_sock.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/ninet_hashtables.h         $KERNEL_BUILD_ROOT/include/net/ninet_hashtables.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nip.h                      $KERNEL_BUILD_ROOT/include/net/nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nip_addrconf.h             $KERNEL_BUILD_ROOT/include/net/nip_addrconf.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nip_fib.h                  $KERNEL_BUILD_ROOT/include/net/nip_fib.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nip_route.h                $KERNEL_BUILD_ROOT/include/net/nip_route.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nip_udp.h                  $KERNEL_BUILD_ROOT/include/net/nip_udp.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/nndisc.h                   $KERNEL_BUILD_ROOT/include/net/nndisc.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/tcp_nip.h                  $KERNEL_BUILD_ROOT/include/net/tcp_nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/net/transp_nip.h               $KERNEL_BUILD_ROOT/include/net/transp_nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/netns/nip.h                $KERNEL_BUILD_ROOT/include/net/netns/nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/flow_nip.h                 $KERNEL_BUILD_ROOT/include/net/flow_nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/if_ninet.h                 $KERNEL_BUILD_ROOT/include/net/if_ninet.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/ninet_connection_sock.h    $KERNEL_BUILD_ROOT/include/net/ninet_connection_sock.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/ninet_hashtables.h         $KERNEL_BUILD_ROOT/include/net/ninet_hashtables.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nip.h                      $KERNEL_BUILD_ROOT/include/net/nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nip_addrconf.h             $KERNEL_BUILD_ROOT/include/net/nip_addrconf.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nip_fib.h                  $KERNEL_BUILD_ROOT/include/net/nip_fib.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nip_route.h                $KERNEL_BUILD_ROOT/include/net/nip_route.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nip_udp.h                  $KERNEL_BUILD_ROOT/include/net/nip_udp.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/nndisc.h                   $KERNEL_BUILD_ROOT/include/net/nndisc.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/tcp_nip.h                  $KERNEL_BUILD_ROOT/include/net/tcp_nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/net/transp_nip.h               $KERNEL_BUILD_ROOT/include/net/transp_nip.h
 	
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/uapi/linux/newip_route.h       $KERNEL_BUILD_ROOT/include/uapi/linux/newip_route.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/uapi/linux/nip.h               $KERNEL_BUILD_ROOT/include/uapi/linux/nip.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/uapi/linux/nip_icmp.h          $KERNEL_BUILD_ROOT/include/uapi/linux/nip_icmp.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/uapi/linux/newip_route.h       $KERNEL_BUILD_ROOT/include/uapi/linux/newip_route.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/uapi/linux/nip.h               $KERNEL_BUILD_ROOT/include/uapi/linux/nip.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/uapi/linux/nip_icmp.h          $KERNEL_BUILD_ROOT/include/uapi/linux/nip_icmp.h
 	
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/include/trace/hooks/nip_hooks.h        $KERNEL_BUILD_ROOT/include/trace/hooks/nip_hooks.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/include/trace/hooks/nip_hooks.h        $KERNEL_BUILD_ROOT/include/trace/hooks/nip_hooks.h
 	
-	ln -s -f $NEWIP_SOURCE_ROOT/code/linux/net/newip                              $KERNEL_BUILD_ROOT/net
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_addr.c                            $KERNEL_BUILD_ROOT/net/newip/nip_addr.c
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_addr.h                            $KERNEL_BUILD_ROOT/net/newip/nip_addr.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_addr.h                            $KERNEL_BUILD_ROOT/include/uapi/linux/nip_addr.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_checksum.c                        $KERNEL_BUILD_ROOT/net/newip/nip_checksum.c
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_checksum.h                        $KERNEL_BUILD_ROOT/net/newip/nip_checksum.h
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_hdr_decap.c                       $KERNEL_BUILD_ROOT/net/newip/nip_hdr_decap.c
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_hdr_encap.c                       $KERNEL_BUILD_ROOT/net/newip/nip_hdr_encap.c
-	ln -s -f $NEWIP_SOURCE_ROOT/code/common/nip_hdr.h                             $KERNEL_BUILD_ROOT/net/newip/nip_hdr.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/linux/net/newip                              $KERNEL_BUILD_ROOT/net
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_addr.c                            $KERNEL_BUILD_ROOT/net/newip/nip_addr.c
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_addr.h                            $KERNEL_BUILD_ROOT/net/newip/nip_addr.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_addr.h                            $KERNEL_BUILD_ROOT/include/uapi/linux/nip_addr.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_checksum.c                        $KERNEL_BUILD_ROOT/net/newip/nip_checksum.c
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_checksum.h                        $KERNEL_BUILD_ROOT/net/newip/nip_checksum.h
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_hdr_decap.c                       $KERNEL_BUILD_ROOT/net/newip/nip_hdr_decap.c
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_hdr_encap.c                       $KERNEL_BUILD_ROOT/net/newip/nip_hdr_encap.c
+	ln -s -f $NEWIP_SOURCE_ROOT/src/common/nip_hdr.h                             $KERNEL_BUILD_ROOT/net/newip/nip_hdr.h
 
 	cd -
 }
